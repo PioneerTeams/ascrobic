2bc2be12ac5ab1513c189b173f9db4457f514bfa
Bart <1714877505@qq.com>
2019-10-14 10:45:06 +0800

    yingxiao

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,11 +2,12 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
-
+console.log(process.env.VUE_APP_BASE_API)
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
+  headers: { 'Authorization': 'Bearer jDtZjl-mjQoAK93159aUcBAdJdof71P3', 'x-org-id': 66229, 'x-org-type': 5, 'x-user-id': 507039579 },
   timeout: 5000 // request timeout
 })
 
