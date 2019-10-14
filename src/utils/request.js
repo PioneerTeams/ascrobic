efb468a669ace6d65aa33a312bcb951216d8aff8
Bart <1714877505@qq.com>
2019-10-14 20:47:14 +0800

    店内促销

--- src/utils/request.js
+++ src/utils/request.js
@@ -46,6 +46,10 @@ service.interceptors.response.use(
   response => {
     const res = response.data
 
+    if (res.code === 200) {
+      return res
+    }
+
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 20000) {
       Message({
